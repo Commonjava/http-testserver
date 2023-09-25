4c7c06c20bec26fbd9a2150b5044c757b0b0bb6a
Gang Li <ligangty@users.noreply.github.com>
2023-09-25 22:23:21 +0800

    Add quarkus based support

---
+++ quarkus/src/main/java/org/commonjava/test/http/quarkus/InjectExpected.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2011-2022 Red Hat, Inc. (https://github.com/Commonjava/http-testserver)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.test.http.quarkus;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * We use this new annotation to let the {@link org.commonjava.test.http.expect.ExpectationServer} have ability to be work like the
+ * old junit Rule based way under Quarkus Based Test.
+ * Note: This annotation should work with @QuarkusTest under a Quarkus based project, like below: <br />
+ *
+ * <pre>
+ *   {@literal @}QuarkusTest
+ *    public class TestClass {
+ *        {@literal @}InjectExpected(base="base")
+ *         ExpectationServer server;
+ *         ......
+ *    }
+ * </pre>
+ */
+@Target( ElementType.FIELD)
+@Retention( RetentionPolicy.RUNTIME)
+public @interface InjectExpected
+{
+    /**
+     * The base path of the server
+     *
+     * @return
+     */
+    String base() default "";
+
+    /**
+     * The default port of the server. Note that if the port is occupied, the server will randomly find a new port to start
+     * @return
+     */
+    int port() default 0;
+}
